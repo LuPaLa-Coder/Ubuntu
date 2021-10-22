12245abc67105562652d891073e5043a224da19d
LuPaLa-Coder <77382170+LuPaLa-Coder@users.noreply.github.com>
2021-10-22 15:58:18 +0200

    Update installDotNet.sh

--- installDotNet.sh
+++ installDotNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Step 1: Download and Install MS Prob on Ubuntu
-wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb
 
 sudo dpkg -i packages-microsoft-prod.deb
 
